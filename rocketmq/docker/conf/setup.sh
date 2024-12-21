1e10a966eadf3bd8c7be32f10554b61810425ab3
Bruce Chen <805899926@qq.com>
2024-12-22 01:59:22 +0800

    CI: sync code

--- rocketmq/docker/conf/setup.sh
+++ rocketmq/docker/conf/setup.sh
@@ -4,7 +4,7 @@ for i in $(seq 1 2);do
  broker_data_dir=/data/${i}
  broker_conf=${broker_data_dir}/broker.conf
  mkdir -p ${broker_data_dir}
-cat << EOF > ${broker_conf}
+ tee ${broker_conf} << EOF
 brokerClusterName = cluster1
 brokerName = rocketmq-broker-${i}
 brokerId = 0
