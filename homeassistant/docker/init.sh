17aaebee106fff437dbb24b1d4f2577570c3eec4
Bruce Chen <805899926@qq.com>
2025-08-30 11:32:30 +0800

    CI: sync code

---
+++ homeassistant/docker/init.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+set -e
+sudo mkdir -p /volume1/storage/docker-data/homeassistant/data/
+sudo cp -rv /volume1/storage/docker-data/homeassistant/conf/* /volume1/storage/docker-data/homeassistant/data/
